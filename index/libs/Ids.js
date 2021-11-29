8177b55c0ea672d810302f302bea5037d19ab756
Edoardo <edoardo.morena@mail.polimi.it>
2021-11-29 15:00:51 +0100

    added file commentati

---
+++ index/libs/Ids.js
@@ -0,0 +1,11 @@
+
+export default class Ids {
+  constructor(prefix = '') {
+    this.id = 0;
+    this.prefix = prefix;
+  }
+
+  next() {
+    const id = `${ this.prefix }_${ this.id++ }`;
+  }
+}
