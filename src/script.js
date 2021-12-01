073c4bb07af58311f53408d5b5f3b1cf8189396a
Edoardo <edoardo.morena@mail.polimi.it>
2021-12-01 17:15:02 +0100

    Aggiunta Waveform senza libs

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 var inputBuffer, currentAudio;  
-var ctx = new AudioContext();
-ctx.resume()
+var c = new AudioContext();
+c.resume()
 
 document.querySelectorAll('.drop_zone_input').forEach(inputElement => {
     const dropZoneElement = inputElement.closest(".drop_zone");
@@ -39,10 +39,10 @@ document.querySelectorAll('.drop_zone_input').forEach(inputElement => {
             var file = e.dataTransfer.files[0];
 
             // conversion to data buffer (inputBuffer)
-            file.arrayBuffer().then((arrayBuffer) => ctx.decodeAudioData(arrayBuffer)).then((decodedAudio) => {
+            file.arrayBuffer().then((arrayBuffer) => c.decodeAudioData(arrayBuffer)).then((decodedAudio) => {
                 inputBuffer = decodedAudio
                 startProcessing()
-                updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+               // updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
             });
         }
 
@@ -52,7 +52,6 @@ document.querySelectorAll('.drop_zone_input').forEach(inputElement => {
 });
 
 function startProcessing() {
-
     // TODO load new page / modify page
     // TODO link to gran js
 
@@ -60,14 +59,15 @@ function startProcessing() {
     
     if (currentAudio != null){
         // if the context was already playing
-        ctx.close()
-        ctx = new AudioContext()
-        ctx.resume()
+        c.close()
+        c = new AudioContext()
+        c.resume()
     }
-    currentAudio = ctx.createBufferSource();
+    currentAudio = c.createBufferSource();
     currentAudio.buffer = inputBuffer;
-    currentAudio.connect(ctx.destination);
-    currentAudio.start(ctx.currentTime); 
+    currentAudio.connect(c.destination);
+    currentAudio.start(c.currentTime); 
+    draw(normalizeData(filterData(inputBuffer)))
 }
 
 /**
@@ -75,6 +75,8 @@ function startProcessing() {
  * @param {HTMLElement} dropZoneElement 
  * @param {File} file 
  */
+
+/*
 function updateThumbnail(dropZoneElement, file) {
 
     let thumbnailElement = dropZoneElement.querySelector('.drop_zone_thumb');
@@ -108,10 +110,112 @@ function updateThumbnail(dropZoneElement, file) {
     }
 
 }
+*/
+
+function filterData(audioBuffer) {
+    const rawData = audioBuffer.getChannelData(0); // We only need to work with one channel of data
+    const samples = 200; // Number of samples we want to have in our final data set
+    const blockSize = Math.floor(rawData.length / samples); // the number of samples in each subdivision
+    const filteredData = [];
+    for (let i = 0; i < samples; i++) {
+      let blockStart = blockSize * i; // the location of the first sample in the block
+      let sum = 0;
+      for (let j = 0; j < blockSize; j++) {
+        sum = sum + Math.abs(rawData[blockStart + j]) // find the sum of all the samples in the block
+      }
+      filteredData.push(sum / blockSize); // divide the sum by the block size to get the average
+    }
+    return filteredData;
+}
+
+function normalizeData(filteredData) {
+    const multiplier = Math.pow(Math.max(...filteredData), -1);
+    return filteredData.map(n => n * multiplier);
+}
+
+function drawLineSegment(ctx, x, y, width, isEven){
+    console.log("qui!!")
+    console.log(ctx)
+    console.log(x)
+    console.log(y)
+    console.log(width)
+    console.log(isEven)
+    ctx.lineWidth = 1; // how thick the line is
+    ctx.strokeStyle = "#F0090E"; // what color our line is
+    ctx.beginPath();
+    y = isEven ? y : -y;
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, y);
+    ctx.arc(x + width / 2, y, width / 2, Math.PI, 0, isEven);
+    ctx.lineTo(x + width, 0);
+    ctx.stroke();
+};
+
+function draw(normalizedData) {
+  // Set up the canvas
+  const canvas = document.getElementById("audiocanvas");
+  const dpr = window.devicePixelRatio || 1;
+  const padding = 20;
+  canvas.width = canvas.offsetWidth * dpr;
+  canvas.height = (canvas.offsetHeight + padding * 2) * dpr;
+  const ctx = canvas.getContext("2d");
+  ctx.scale(dpr, dpr);
+  ctx.translate(0, canvas.offsetHeight / 2 + padding); // Set Y = 0 to be in the middle of the canvas
+
+  // draw the line segments
+  const width = canvas.offsetWidth / normalizedData.length;
+  for (let i = 0; i < normalizedData.length; i++) {
+    const x = width * i;
+    let height = normalizedData[i] * canvas.offsetHeight - padding;
+    if (height < 0) {
+        height = 0;
+    } else if (height > canvas.offsetHeight / 2) {
+        height = height > canvas.offsetHeight / 2;
+    }
+    drawLineSegment(ctx, x, height, width, (i + 1) % 2);
+  }
+};
 
 
 
+/*
 
+const canvas = document.getElementById("audiocanvas");
+canvasCtx = canvas.getContext("2d");
+var bindex = 0;
+
+function drawBuffer() {
+    canvasCtx.beginPath();
+    canvasCtx.clearRect(0,0,canvas.width,canvas.height);
+    canvasCtx.moveTo(0,canvas.height/2);
+    dataIn = inputBuffer.getChannelData(0)
+    step = canvas.width/inputBuffer.length;
+    for(var i=0;i<inputBuffer.length;i++) {
+        canvasCtx.lineTo(Math.round(i*step), 
+            canvas.height/2+200*dataIn[i])};
+    
+    markPosition = Math.round((bindex % inputBuffer.length)/step)
+    canvasCtx.moveTo(markPosition, 0)
+    canvasCtx.lineTo(markPosition, canvas.height)
+    canvasCtx.stroke();
+
+    //draw a couple of vertical lines corresponding to sampling start and end
+
+    
+    canvasCtx.strokeStyle = "#00FF00"
+    canvasCtx.beginPath()
+    canvasCtx.moveTo(canvas.width*sliceStart,0);
+    canvasCtx.lineTo(canvas.width*sliceStart,canvas.height)
+    canvasCtx.moveTo(canvas.width*sliceEnd,0);
+    canvasCtx.lineTo(canvas.width*sliceEnd,canvas.height)
+    canvasCtx.stroke()
+    canvasCtx.strokeStyle = "#000000"
+    
+
+    window.requestAnimationFrame(drawBuffer)
+}
+
+*/
 
 
 /*
