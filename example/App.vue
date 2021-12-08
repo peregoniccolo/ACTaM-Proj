7aa636346e4ce11045ff28a5849e288e3aa808fb
Niccolò Perego <niccolo.perego@mail.polimi.it>
2021-12-08 17:09:36 +0100

    knob things

---
+++ example/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <knob-control v-model="someValue"></knob-control>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import KnobControl from "vue-knob-control";
+Vue.use(KnobControl);
+Vue.config.productionTip = false;
+
+console.log(3);
+
+export default {
+  name: "App",
+  components: {
+    KnobControl,
+  },
+  data() {
+    return {
+      someValue: 22,
+    };
+  },
+};
+
+console.log(4);
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- example/example.js
+++ example/example.js
@@ -3,100 +3,109 @@ import Granular from '../libs/Granular/Granular';
 import p5 from 'p5';
 import 'p5/lib/addons/p5.sound';
 
+import Vue from "vue";
+import App from "./App.vue";
+
+window.onload = function () {
+	new Vue({
+		render: h => h(App)
+	}).$mount("#app");
+}
+
 //PROCESSO GENERALE: creo un granular con vari parametri, gli passo un bufer audio con setBuffer, al click di un bottone faccio startVoice (passando posizione iniziale e volume)
 // questa funzione crea e fa suonare delle Voice (che a loro volta creano e fanno suonare dei grain).
 
 //IN QUESTO CASO: ogni tot ms abasso il pitch dei grain
 
 async function getData(url) { //funzione da modificare in modo da prendere il file drag & droppato dall'utente
-  return new Promise((resolve) => {
-    const request = new XMLHttpRequest();
+	return new Promise((resolve) => {
+		const request = new XMLHttpRequest();
 
-    request.open('GET', url, true);
+		request.open('GET', url, true);
 
-    request.responseType = 'arraybuffer';
+		request.responseType = 'arraybuffer';
 
-    request.onload = function () {
-      const audioData = request.response;
+		request.onload = function () {
+			const audioData = request.response;
 
-      resolve(audioData);
-    }
+			resolve(audioData);
+		}
 
-    request.send();
-  });
+		request.send();
+	});
 }
 
 async function init() {
-  const audioContext = p5.prototype.getAudioContext();
+	const audioContext = p5.prototype.getAudioContext();
 
-  const granular = new Granular({
-    audioContext,
-    envelope: {
-      attack: 0,
-      release: 0.5
-    },
-    density: 0.1,
-    spread: 0.1,
-    pitch: 1
-  });
+	const granular = new Granular({
+		audioContext,
+		envelope: {
+			attack: 0,
+			release: 0.5
+		},
+		density: 0.1,
+		spread: 0.1,
+		pitch: 1
+	});
 
-  //usa p5.js che è molto simile a WebAudio
-  const delay = new p5.Delay();
+	//usa p5.js che è molto simile a WebAudio
+	const delay = new p5.Delay();
 
-  delay.process(granular, 0.5, 0.5, 3000); // source, delayTime, feedback, filter frequency
+	delay.process(granular, 0.5, 0.5, 3000); // source, delayTime, feedback, filter frequency
 
-  const reverb = new p5.Reverb();
+	const reverb = new p5.Reverb();
 
-  // due to a bug setting parameters will throw error
-  // https://github.com/processing/p5.js/issues/3090
-  reverb.process(delay); // source, reverbTime, decayRate in %, reverse
+	// due to a bug setting parameters will throw error
+	// https://github.com/processing/p5.js/issues/3090
+	reverb.process(delay); // source, reverbTime, decayRate in %, reverse
 
-  reverb.amp(3);
+	reverb.amp(3);
 
-  const compressor = new p5.Compressor();
+	const compressor = new p5.Compressor();
 
-  compressor.process(reverb, 0.005, 6, 10, -24, 0.05); // [attack], [knee], [ratio], [threshold], [release]
+	compressor.process(reverb, 0.005, 6, 10, -24, 0.05); // [attack], [knee], [ratio], [threshold], [release]
 
-  granular.on('settingBuffer', () => console.log('setting buffer'));
-  granular.on('bufferSet', () => console.log('buffer set'));
-  granular.on('grainCreated', () => console.log('grain created'));
+	granular.on('settingBuffer', () => console.log('setting buffer'));
+	granular.on('bufferSet', () => console.log('buffer set'));
+	granular.on('grainCreated', () => console.log('grain created'));
 
-  const data = await getData('example.wav');
-  console.log(data)
+	const data = await getData('example.wav');
+	console.log(data)
 
-  await granular.setBuffer(data);
+	await granular.setBuffer(data);
 
-  const resume = document.getElementById('resume');
+	const resume = document.getElementById('resume');
 
-  resume.addEventListener('click', () => {
-    const id = granular.startVoice({ //passo a startVoice una posizione e un volume, lei la passerà a sua volta a una voice che viene creata al suo interno 
-      //(guarda Granular.js -> startVoice).
-      //Nel momento in cui viene chiamata play su questa voice essa creerà e suonerà un grain nel modo opportuno (guarda Granular.js -> createGain)
+	resume.addEventListener('click', () => {
+		const id = granular.startVoice({ //passo a startVoice una posizione e un volume, lei la passerà a sua volta a una voice che viene creata al suo interno 
+			//(guarda Granular.js -> startVoice).
+			//Nel momento in cui viene chiamata play su questa voice essa creerà e suonerà un grain nel modo opportuno (guarda Granular.js -> createGain)
 
-      position: 0.1,
-      volume: 0.5
-    });
+			position: 0.1,
+			volume: 0.5
+		});
 
-    let pitch = 1;
+		let pitch = 1;
 
-    const interval = setInterval(() => { //ogni 200ms abbasso il pitch di 0.05, esso andrà a influire il playbackRate.value del buffer (guarda Granular.js -> createGain)
-      pitch -= 0.05;
+		const interval = setInterval(() => { //ogni 200ms abbasso il pitch di 0.05, esso andrà a influire il playbackRate.value del buffer (guarda Granular.js -> createGain)
+			pitch -= 0.05;
 
-      granular.set({
-        pitch
-      });
-    }, 200);
+			granular.set({
+				pitch
+			});
+		}, 200);
 
-    setTimeout(() => {
-      clearInterval(interval);
+		setTimeout(() => {
+			clearInterval(interval);
 
-      granular.stopVoice(id);
+			granular.stopVoice(id);
 
-      granular.set({
-        pitch: 1
-      });
-    }, 2000);
-  })
+			granular.set({
+				pitch: 1
+			});
+		}, 2000);
+	})
 }
 
 init();
